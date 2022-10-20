b7fdc8f34c1f79ccda11be2e9cc90a8ee2c0d73e
Kyrylo Salomatin <kirill.salomatin@itperspectives.ro>
2022-10-20 18:24:32 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateExample
+{
+    public class Program
+    {
+        public delegate void Stndart();
+
+        static void Main(string[] args)
+        {
+            StockExchangeMonitor stockExchangeMonitor = new StockExchangeMonitor();
+            stockExchangeMonitor.PriceChangeHandler = ShowPrice;
+            stockExchangeMonitor.Start();
+
+
+        }
+
+        public static void ShowPrice(int price)
+        {
+            Console.WriteLine($"New price is : {price}");
+        }
+    }
+}
---
+++ StockExchangeMonitor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DelegateExample
+{
+    public class StockExchangeMonitor
+    {
+        public delegate void PriceChanger (int price);
+
+        public PriceChanger PriceChangeHandler { get; set; }
+
+        public void Start()
+        {
+            while (true)
+            {
+                int bankOfAmericaPrice = (new Random()).Next(70, 110);
+                PriceChangeHandler(bankOfAmericaPrice);
+                Thread.Sleep(2500);
+            }
+        }
+                
+
+
+    }
+}
